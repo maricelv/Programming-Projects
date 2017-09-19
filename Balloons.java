f9b12b5a35db30a78afc4ff360d363c91b844d2a
maricelv <vicmaricel@gmail.com>
2017-09-19 12:20:16 -0700

    Update Balloons.java

--- Balloons.java
+++ Balloons.java
@@ -13,23 +13,21 @@ public class Balloons extends JApplet
 
 	public void paint(Graphics page)
 	{
-		final int MID = 50;
-		final int TOP = 150;
 
 		setBackground(Color.cyan);
 
 		
 		page.setColor(Color.yellow);
 		page.fillOval(150, 40, 45, 60); 	// yellow balloon
-		page.drawLine(MID+25, TOP+60, MID+55, TOP+60);	// string
+		page.drawLine(50,50,50,175);	// string
 
 		page.setColor(Color.red);
 		page.fillOval(25, 40, 45, 60); 	// red balloon
-		page.drawLine(MID+25, TOP+60, MID+55, TOP+60);	// string
+		page.drawLine(50,50,50,175);	// string
 		
 		page.setColor(Color.blue);
 		page.fillOval(260, 40, 45, 60); 	// blue one
-		page.drawLine(MID+25, TOP+60, MID+55, TOP+60);	// string
+		page.drawLine(50,50,50,175);	// string
 		
 	
 		
